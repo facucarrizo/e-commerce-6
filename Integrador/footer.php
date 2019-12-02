4aa62c35843e85aa4acc6fddb992bf311fcb7514
Luciano <lucho.aste@gmail.com>
2019-12-02 16:19:19 -0300

    PHP
    Cambio index html a php y creado requiere de footer

---
+++ Integrador/footer.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/67f61afa3e.js" crossorigin="anonymous"></script>
+    <title>CASBUSAGI - Zapatillas al paso</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+
+
+
+
+
+<?php require_once 'footer.php';  ?>
+
+    <!-- Footer-->
+    <footer class="page-footer font-small unique-color-dark">
+      <div class="footer">
+        <div class="container">
+
+          <div class="row py-4 d-flex align-items-center" ;>
+
+
+            <div class="col-md-12 col-lg-12 text-center text-md-right">
+              <!-- Facebook -->
+              <a target="_blank" href=http://www.facebook.com class="w-ico fb-ic" style="text-decoration:none" id="facebook">
+                <i class="fab fa-facebook-f white-text mr-4"> </i>
+              </a>
+              <!-- Twitter -->
+              <a target="_blank" href=http://twitter.com class="w-ico tw-ic" style="text-decoration:none" id="twitter">
+                <i class="fab fa-twitter white-text mr-4"> </i>
+              </a>
+
+              <!--Linkedin -->
+              <a target="_blank" href=https://ar.linkedin.com/ class="w-ico li-ic" style="text-decoration:none" id="linkedin">
+                <i class="fab fa-linkedin-in white-text mr-4"> </i>
+              </a>
+              <!--Instagram-->
+              <a target="_blank" href=https://www.instagram.com/ class="w-ico ins-ic" style="text-decoration:none" id="instagram">
+                <i class="fab fa-instagram white-text"> </i>
+              </a>
+            </div>
+             </div> </div> </div> <div class="container text-center text-md-left mt-5 ">
+              <div class="row mt-3">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                  <h4 class="text-uppercase font-weight-bold">CASBUSAGI</h4>
+                  <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                  <p>
+                    <a href="#" style="text-decoration:none">Nuestra historia</a>
+                  </p>
+                  <p>
+                    <a href="Formulario Registro.html" style="text-decoration:none">Como suscribirse</a>
+                  </p>
+                  <p>
+                    <a href="faq.html" style="text-decoration:none">Preguntas frecuentes</a>
+                  </p>
+                  <p>
+                    <a href="#" style="text-decoration:none">Trabaja en nuestro equipo</a>
+                  </p>
+                  <p>
+                    <a href="#" style="text-decoration:none">Guía de talles</a>
+                  </p>
+                </div>
+
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <!-- Links -->
+                  <h6 class="text-uppercase font-weight-bold">Productos</h6>
+                  <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                  <p>
+                    <a href="producto.html" style="text-decoration:none">Nike</a>
+                  </p>
+                  <p>
+                    <a href="producto-adidas.html" style="text-decoration:none">Adidas</a>
+                  </p>
+                  <p>
+                    <a href="producto-vans.html" style="text-decoration:none">Vans</a>
+                  </p>
+
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 p-0" >
+                  <h6 class="text-uppercase font-weight-bold">Contacto</h6>
+                  <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                  <p>
+                    <i class="fas fa-envelope mr-3"></i> <a href="mailto:info@casbusagi.com" style="text-decoration:none">info@casbusagi.com</a>
+                  </p>
+                  <p>
+                    <i class="fas fa-phone mr-3"></i> <a href="tel:+54 234 567 88" style="text-decoration:none"> +54 234 567 88</a>
+                  </p>
+                  <!-- <p>
+                    <a href="#!">Mi cuenta</a>
+                  </p>
+                  <p>
+                    <a href="#!">Become an Affiliate</a>
+                  </p>
+                  <p>
+                    <a href="#!">Tasas de envío</a>
+                  </p>
+                  <p>
+                    <a href="#!">Ayuda</a>
+                  </p> -->
+
+
+                </div>
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+                  <h6 class="text-uppercase font-weight-bold"></h6>
+                  <!-- <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;"> -->
+                    <p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.3313139806187!2d-58.38386198505181!3d-34.62106686592268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccb28ea8781cb%3A0x950feb519009506e!2sLima%201111%2C%20C1073AAW%20CABA!5e0!3m2!1ses-419!2sar!4v1574711262818!5m2!1ses-419!2sar" width="250" height="250" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                    </p>
+                </div>
+              </div>
+          </div>
+
+          <!-- Copyright -->
+          <div class="footer-copyright text-center py-3">© 2019 Copyright. Todos los derechos reservados.
+            <a href="#" style="text-decoration:none">Politica de privacidad</a>
+          </div>
+          <!-- Copyright -->
+
+    </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+      crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+      crossorigin="anonymous"></script>
+  </body>
+</html>
